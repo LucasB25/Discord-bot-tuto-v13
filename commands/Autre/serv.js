30cb3144264579eb9a23c582d2376402e95dfca9
Lucasmisa0 <50886682+Lucasmisa0@users.noreply.github.com>
2019-07-15 13:29:01 +0200

    Delete serv.js

--- commands/Autre/serv.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async (bot, message, args, cmd) => {
-        message.reply("**Voici le lien d'invitation pour rejoindre Delta - Infinity**: https://discord.gg/pkqHtgy");
-}
-module.exports.help = {
-name: 'serv',
-aliases: []
-};
