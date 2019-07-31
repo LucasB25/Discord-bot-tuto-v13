1f9282d3c13b7112a032f1b32b34012e5f45ab9b
Lucasmisa0 <50886682+Lucasmisa0@users.noreply.github.com>
2019-07-31 19:29:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,18 @@ module.exports.bot = bot;
 bot.on("ready", () => {
     console.log(`Je suis prêt à être utilisé.`)})
 
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Launcher 1.7.10";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.largeImageKey = "heaven_logo";
+    discordPresence.smallImageKey = "unnamed";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 0;
+    discordPresence.partyMax = 0;
+    Discord_UpdatePresence(&discordPresence);
+}
+
 bot.login(token);
